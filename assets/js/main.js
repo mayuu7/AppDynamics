127cc7fc163e0d26776e21c20ea34d06b07f1b3d
mayuu7 <jainmayank0@gmail.com>
2019-01-11 02:25:44 +0530

    added and modified the css and js of the registration and the FAQ page

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,11 +35,11 @@ jQuery(document).ready(function ($) {
         nextArrow:"<i class='fa fa-angle-right nextprevright'></i>"
     });
 
-//    $(".study_slider").slick({
-//        dots: true,
-//        slidesToShow: 1,
-//        slidesToScroll: 1
-//    });
+   $(".study_slider").slick({
+       dots: true,
+       slidesToShow: 1,
+       slidesToScroll: 1
+   });
     $(".study_slider").slick({
         dots: true,
         arrows:false,
@@ -255,9 +255,9 @@ $(document).on("scroll", function () {
 
   // ADD SWIPE SUPPORT FOR TOUCH DEVICES
   function setSwipeFn(tl, prev, next) {
-    const hammer = new Hammer(tl);
-    hammer.on("swipeleft", () => next.click());
-    hammer.on("swiperight", () => prev.click());
+    // const hammer = new Hammer(tl);
+    // hammer.on("swipeleft", () => next.click());
+    // hammer.on("swiperight", () => prev.click());
   }
 
   // ADD BASIC KEYBOARD FUNCTIONALITY
@@ -279,3 +279,45 @@ $(document).on("scroll", function () {
   }
 
 })();
+
+
+(function() {
+                // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+                if (!String.prototype.trim) {
+                    (function() {
+                        // Make sure we trim BOM and NBSP
+                        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+                        String.prototype.trim = function() {
+                            return this.replace(rtrim, '');
+                        };
+                    })();
+                }
+
+                [].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+                    // in case the input is already filled..
+                    if( inputEl.value.trim() !== '' ) {
+                        classie.add( inputEl.parentNode, 'input--filled' );
+                    }
+
+                    // events:
+                    inputEl.addEventListener( 'focus', onInputFocus );
+                    inputEl.addEventListener( 'blur', onInputBlur );
+                } );
+
+                function onInputFocus( ev ) {
+                    classie.add( ev.target.parentNode, 'input--filled' );
+                }
+
+                function onInputBlur( ev ) {
+                    if( ev.target.value.trim() === '' ) {
+                        classie.remove( ev.target.parentNode, 'input--filled' );
+                    }
+                }
+            })();
+
+
+           (function() {
+                [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {    
+                    new SelectFx(el);
+                } );
+            })();
